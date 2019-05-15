64c3f3d444d637f7d4c50ac7908804cbf0a5a4bd
mohanteja1 <mohantejachitturi1@gmail.com>
2019-05-15 12:20:19 +0530

    Stack completed

--- src/week2/Collections/List/LinkedList/LinkedListBasics.java
+++ src/week2/Collections/List/LinkedList/LinkedListBasics.java
@@ -108,7 +108,7 @@ public class LinkedListBasics {
             System.out.println(human.toString());
         });
 
-         
+
 
 
 
---
+++ src/week2/Collections/List/Stack/StackBasics.java
@@ -0,0 +1,67 @@
+package week2.Collections.List.Stack;
+import week2.Collections.List.LinkedList.*;
+
+import java.util.Stack;
+
+public class StackBasics {
+
+    public static void main(String arg[]){
+
+
+        //initialization  no primitive types or use wrapper classes
+
+        Stack<Integer> integersStack = new Stack<Integer>();
+
+        integersStack.push(new Integer(23));
+        integersStack.push(new Integer(33));
+        integersStack.push(new Integer(13));
+        integersStack.push(new Integer(63));
+        integersStack.push(new Integer(23));
+        integersStack.push(new Integer(73));
+        integersStack.push(new Integer(93));
+
+        integersStack.forEach(integer ->{
+
+            System.out.println(integer.toString());
+
+        });
+
+        integersStack.pop();
+
+
+
+
+
+
+
+
+        //important methods:
+        // boolean empty()
+
+
+
+        //Object peek( )
+
+        System.out.println(integersStack.peek());
+
+
+
+
+        //Object pop( )
+
+
+
+
+        //Object push(Object element)
+
+        //int search(Object element)
+
+
+        System.out.println(integersStack.search(new Integer(22)));
+
+
+
+    }
+
+
+}
